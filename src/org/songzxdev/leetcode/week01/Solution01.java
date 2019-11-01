3f03e6dd87453873ef41117d89469e2b22c97ce9
songzxDev <songzx_2326@163.com>
2019-11-01 11:01:23 +0800

    提交leetcode java版本代码

---
+++ src/org/songzxdev/leetcode/week01/Solution01.java
@@ -0,0 +1,21 @@
+package org.songzxdev.leetcode.week01;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution01 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(16);
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+            if (map.containsKey(comp) && map.get(comp) != i) {
+                return new int[]{i, map.get(comp)};
+            }
+        }
+        return new int[]{};
+    }
+}
