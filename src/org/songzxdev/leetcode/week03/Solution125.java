020369980098d9a4bb40d4d3e47bab8e2eb372ba
songzxDev <songzx_2326@163.com>
2019-11-15 08:33:28 +0800

    /**      * 题目：125.验证回文串      * 标签：字符串 双指针      *      * @param s      * @return      */     public boolean isPalindrome(String s) {         if (s == null) {             return false;         }         s = s.replaceAll("[^0-9a-zA-Z]", "").toLowerCase();         int i = 0, j = s.length() - 1;         while (i <= j) {             if (s.charAt(i++) != s.charAt(j--)) {                 return false;             }         }         return true;     }

---
+++ src/org/songzxdev/leetcode/week03/Solution125.java
@@ -0,0 +1,34 @@
+package org.songzxdev.leetcode.week03;
+
+//给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+// 说明：本题中，我们将空字符串定义为有效的回文串。
+// 示例 1:
+// 输入: "A man, a plan, a canal: Panama"
+//输出: true
+// 示例 2:
+// 输入: "race a car"
+//输出: false
+// Related Topics 双指针 字符串
+
+public class Solution125 {
+    /**
+     * 题目：125.验证回文串
+     * 标签：字符串 双指针
+     *
+     * @param s
+     * @return
+     */
+    public boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
+        s = s.replaceAll("[^0-9a-zA-Z]", "").toLowerCase();
+        int i = 0, j = s.length() - 1;
+        while (i <= j) {
+            if (s.charAt(i++) != s.charAt(j--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
