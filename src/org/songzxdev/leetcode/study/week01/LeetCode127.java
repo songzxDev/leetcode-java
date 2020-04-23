13c39e0b539617de6133103b614ea475ab8b1ef4
songzxDev <songzx_2326@163.com>
2020-04-23 09:29:30 +0800

    LeetCode127.单词接龙，双向BSF

---
+++ src/org/songzxdev/leetcode/study/week01/LeetCode127.java
@@ -0,0 +1,86 @@
+package org.songzxdev.leetcode.study.week01;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord 的最短转换序列的长度。转换需遵循如下规则：
+//
+//
+//
+// 每次转换只能改变一个字母。
+// 转换过程中的中间单词必须是字典中的单词。
+//
+//
+// 说明:
+//
+//
+// 如果不存在这样的转换序列，返回 0。
+// 所有单词具有相同的长度。
+// 所有单词只由小写字母组成。
+// 字典中不存在重复的单词。
+// 你可以假设 beginWord 和 endWord 是非空的，且二者不相同。
+//
+//
+// 示例 1:
+//
+// 输入:
+//beginWord = "hit",
+//endWord = "cog",
+//wordList = ["hot","dot","dog","lot","log","cog"]
+//
+//输出: 5
+//
+//解释: 一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+//     返回它的长度 5。
+//
+//
+// 示例 2:
+//
+// 输入:
+//beginWord = "hit"
+//endWord = "cog"
+//wordList = ["hot","dot","dog","lot","log"]
+//
+//输出: 0
+//
+//解释: endWord "cog" 不在字典中，所以无法进行转换。
+// Related Topics 广度优先搜索
+public class LeetCode127 {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordSet.contains(endWord)) {
+            return 0;
+        }
+        Set<String> beginSet = new HashSet<>(), endSet = new HashSet<>(), visited = new HashSet<>();
+        int len = 1;
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            Set<String> nextSet = new HashSet<>();
+            for (String word : beginSet) {
+                char[] chrs = word.toCharArray();
+                for (int i = 0; i < chrs.length; i++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        char old = chrs[i];
+                        chrs[i] = c;
+                        String target = String.valueOf(chrs);
+                        if (endSet.contains(target)) {
+                            return len + 1;
+                        }
+                        if (!visited.contains(target) && wordSet.contains(target)) {
+                            nextSet.add(target);
+                            visited.add(target);
+                        }
+                        chrs[i] = old;
+                    }
+                }
+            }
+            beginSet = nextSet.size() < endSet.size() ? nextSet : endSet;
+            endSet = beginSet.size() < endSet.size() ? endSet : nextSet;
+            len++;
+        }
+        return 0;
+    }
+}
